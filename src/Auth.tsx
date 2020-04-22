c665723eada742f8dbd48609390ab5ecffa12b48
Philippe Bourdages <philippe.bourdages@gmail.com>
2020-04-21 21:38:38 -0400

    Remove crappy html from sign in

--- src/Auth.tsx
+++ src/Auth.tsx
@@ -40,8 +40,6 @@ const Auth = () => {
     if (!isSignedIn) {
         return (
             <div>
-                <h1>My App</h1>
-                <p>Please sign-in:</p>
                 <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
             </div>
         );
